b5f15bea2e1db0db507236deea08ebf41fe7b8db
Qin Guan <qinguan20040914@gmail.com>
2022-12-21 01:19:22 +0800

    fix

--- app/(default)/find/head.tsx
+++ app/(default)/find/head.tsx
@@ -4,6 +4,7 @@ export default function FindHead() {
       <link
         href={"https://api.mapbox.com/mapbox-gl-js/v2.11.0/mapbox-gl.css"}
         rel={"stylesheet"}
+        // @ts-ignore
         precedence="default"
       />
     </>
