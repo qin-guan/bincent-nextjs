be1f411de244e2a006ecd6f6bde11d27599c0654
Qin Guan <qinguan20040914@gmail.com>
2022-12-21 23:26:51 +0800

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 }) {
 
   return (
-    <html lang="en">
+    <html lang="en" data-theme={"pastel"}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
